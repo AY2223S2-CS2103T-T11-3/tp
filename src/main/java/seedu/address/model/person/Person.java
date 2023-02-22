e4184ebfb71aa4c12b5fcd6714f74293d25b473b
EksdeeeX <Bryantanzhongkang@gmail.com>
2023-02-23 04:39:51 +0800

    Tutorial 2-add command

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import seedu.address.logic.commands.RemarkCommand;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -23,16 +24,17 @@ public class Person {
     // Data fields
     private final Address address;
     private final Set<Tag> tags = new HashSet<>();
-
+    private final Remark remark;
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+    public Person(Name name, Phone phone, Email email, Address address, Remark remark, Set<Tag> tags) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
+        this.remark = remark;
         this.tags.addAll(tags);
     }
 
@@ -50,6 +52,9 @@ public class Person {
 
     public Address getAddress() {
         return address;
+    }
+    public Remark getRemark() {
+        return remark;
     }
 
     /**
@@ -101,22 +106,19 @@ public class Person {
         return Objects.hash(name, phone, email, address, tags);
     }
 
-    @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
         builder.append(getName())
-                .append("; Phone: ")
+                .append(" Phone: ")
                 .append(getPhone())
-                .append("; Email: ")
+                .append(" Email: ")
                 .append(getEmail())
-                .append("; Address: ")
-                .append(getAddress());
-
-        Set<Tag> tags = getTags();
-        if (!tags.isEmpty()) {
-            builder.append("; Tags: ");
-            tags.forEach(builder::append);
-        }
+                .append(" Address: ")
+                .append(getAddress())
+                .append(" Remark: ")
+                .append(getRemark())
+                .append(" Tags: ");
+        getTags().forEach(builder::append);
         return builder.toString();
     }
 
